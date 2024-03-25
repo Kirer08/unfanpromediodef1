fd5786c02c6cb4acd1665525409500ed5a306e2d
Kirer08 <juanenrique04@gmail.com>
2024-03-25 09:28:50 +0100

    ampliar imagen
    aun sin efecto

---
+++ Script/script.js
@@ -0,0 +1,8 @@
+// script.js
+function ampliarImagen(imagen) {
+    if (imagen.className == "ampliada") {
+        imagen.className = "mid1";
+    } else {
+        imagen.className = "ampliada";
+    }
+}

e8f38b8abc559142ab58369ee0374c0a21cfef20
Kirer08 <juanenrique04@gmail.com>
2024-06-05 21:03:30 +0200

    refresh

--- Script/script.js
+++
@@ -1,8 +0,0 @@
-function ampliarImagen(id) {
-    var imagen = document.getElementById(id);
-    if (imagen.className.includes("ampliada")) {
-        imagen.classList.remove("ampliada");
-    } else {
-        imagen.classList.add("ampliada");
-    }
-}
